8caf6cdb551e9dd2eba847130cbe16140fa24032
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-12-23 20:11:54 +0330

    similar product

--- website/templatetags/product_tags.py
+++ website/templatetags/product_tags.py
@@ -5,12 +5,10 @@ register = template.Library()
 
 @register.inclusion_tag('includes/similar_prod.html', takes_context=True)
 def show_similar_products(context, product):
-    request = context['request']
     similar_products = ProductModel.objects.filter(
-        category__in=product.category.all()
+        category=product.category
     ).exclude(id=product.id).order_by('-created_date')[:4]
     context.update({
         'similar_products': similar_products,
-        'request': request
     })
     return context
